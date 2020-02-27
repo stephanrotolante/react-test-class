776afd404bb02e0dd7e7f175319fe4d96304a867
stephanrotolante <stephanrotolante@gmail.com>
2020-02-26 22:07:02 -0500

    first commit

---
+++ projects/Section 1/create-react-app/demo/src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App';
+
+
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+test('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toBeTruthy();
+});
